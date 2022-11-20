08296de9195956a2d6d79be56b9496ae4e1c45b2
aetos <aetos382@users.noreply.github.com>
2022-11-21 00:55:40 +0900

    implement operators interfaces

---
+++ Scru128DotNet/Scru128.Operators.Net70.cs
@@ -0,0 +1,13 @@
+﻿#if NET7_0_OR_GREATER
+
+using System.Numerics;
+
+namespace Scru128DotNet;
+
+public readonly partial struct Scru128 :
+    IEqualityOperators<Scru128, Scru128, bool>,
+    IComparisonOperators<Scru128, Scru128, bool>
+{
+}
+
+#endif
--- Scru128DotNet/Scru128.Operators.cs
+++ Scru128DotNet/Scru128.Operators.cs
@@ -3,43 +3,43 @@
 public readonly partial struct Scru128
 {
     public static bool operator ==(
-        in Scru128 left,
-        in Scru128 right)
+        Scru128 left,
+        Scru128 right)
     {
         return left.Equals(right);
     }
 
     public static bool operator !=(
-        in Scru128 left,
-        in Scru128 right)
+        Scru128 left,
+        Scru128 right)
     {
         return !left.Equals(right);
     }
 
     public static bool operator <(
-        in Scru128 left,
-        in Scru128 right)
+        Scru128 left,
+        Scru128 right)
     {
         return left.CompareTo(right) < 0;
     }
 
     public static bool operator <=(
-        in Scru128 left,
-        in Scru128 right)
+        Scru128 left,
+        Scru128 right)
     {
         return left.CompareTo(right) <= 0;
     }
 
     public static bool operator >(
-        in Scru128 left,
-        in Scru128 right)
+        Scru128 left,
+        Scru128 right)
     {
         return left.CompareTo(right) > 0;
     }
 
     public static bool operator >=(
-        in Scru128 left,
-        in Scru128 right)
+        Scru128 left,
+        Scru128 right)
     {
         return left.CompareTo(right) >= 0;
     }
