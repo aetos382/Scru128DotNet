d8aeb2c8f7a1f323145844a897ca494c8c32d75f
aetos <aetos382@users.noreply.github.com>
2022-11-23 18:30:50 +0900

    test

--- ship.config.js
+++ ship.config.js
@@ -1,1 +1,27 @@
-module.exports = { buildCommand: () => null };
+const fs = require('fs');
+const path = require('path');
+const { XMLParser, XMLBuilder, XMLValidator } = require('fast-xml-parser');
+
+module.exports = {
+
+    versionUpdated: ({version, releaseType, dir, exec}) => {
+        const directoryBuildPropsPath = path.resolve(dir, 'Directory.Build.props');
+        const xmlSource = fs.readFileSync(directoryBuildPropsPath, 'utf-8').toString();
+
+        const parser = new XMLParser({
+            ignoreAttributes: false,
+            preserveOrder: true
+        });
+
+        const xml = parser.parse(xmlSource);
+
+        console.log(JSON.stringify(xml));
+
+        const builder = new XMLBuilder({
+            ignoreAttributes: false,
+            preserveOrder: true,
+            format: true
+        })
+    },
+    buildCommand: () => null
+};
