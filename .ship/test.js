16c970d472c6631d6ce0c23b5a48ab895b40fd8e
aetos <aetos382@users.noreply.github.com>
2024-05-20 19:36:39 +0900

    remove shipjs

--- .ship/test.js
+++
@@ -1,5 +0,0 @@
-const path = require('node:path');
-const dir = path.normalize(path.join(__dirname, '..'));
-
-const { setVersion } = require('./setVersion.js');
-setVersion('1.2.3.4', dir);
--- ship.config.js
+++
@@ -1,9 +0,0 @@
-const { setVersion } = require('./.ship/setVersion.js');
-
-module.exports = {
-    versionUpdated: ({version, releaseType, dir, exec}) => {
-        setVersion(version, dir);
-    },
-
-    buildCommand: () => null
-};
